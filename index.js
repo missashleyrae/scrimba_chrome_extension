a2898871e6cc506db0279419a6cef1f147667ac0
Ashley S. <198093402+missashleyrae@users.noreply.github.com>
2025-03-29 15:11:51 +0800

    add save input functionality and update link styling

---
+++ index.js
@@ -0,0 +1,30 @@
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+const inputBtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
+
+function inputButton(){
+    console.log("Button clicked!")
+}
+
+
+inputBtn.addEventListener ("click", function () {
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value=""
+})
+
+function renderLeads(){
+    let listItems = ""
+    for (let i=0; i< myLeads.length; i++) {
+    //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems+=`
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+    }
+ulEl.innerHTML = listItems
+}
